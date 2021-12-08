5ddb55a366fb95214a821cc46029219d586b569d
Abdelwareth <a.mohammedss2050@gmail.com>
2021-12-08 12:25:55 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
+//import { NgModule } from '@angular/core';
+ import { BrowserModule } from '@angular/platform-browser';
+ import { AppRoutingModule } from './app-routing.module';
+ import { AppComponent } from './app.component';
+ import { SimpleModalModule } from 'ngx-simple-modal';
+// import { CalenderComponent } from './components/calender/calender.component';
+ import { CompComponent } from './components/comp/comp.component';
+ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { CalendarModule, DateAdapter } from 'angular-calendar';
+// import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalenderComponent } from './components/calender/calender.component';
+import {NgxPrintModule} from 'ngx-print';
+import { ModalModule } from 'ngb-modal';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalenderComponent,
+    CompComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [ReactiveFormsModule,SimpleModalModule.forRoot({container:document.body}),NgxPrintModule,
+    ModalModule ,
+  CommonModule,
+    FormsModule,
+    NgbModalModule,
+   FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+ 
+}), BrowserModule,
+AppRoutingModule,NgbModule,ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
